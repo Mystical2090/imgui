1421aa9e15c66ec314f5a95baf2f4dbbf592380c
Mystical433 <peterpanlee2015@gmail.com>
2025-07-08 18:55:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,23 @@
 #include <Geode/Geode.hpp>
 #include <imgui-cocos.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
-#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-static bool noclipEnabled = false;
-static bool jumpHackEnabled = false;
-static bool autoclickerEnabled = false;
-static float speedhackValue = 1.0f;
+bool noclipEnabled = false;
+bool jumpHackEnabled = false;
+bool autoclickerEnabled = false;
+float speedhackValue = 1.0f;
 
-class $modify(NoclipHack, PlayLayer) {
-    void destroyPlayer(PlayerObject* player, GameObject* object) {
-        if (!noclipEnabled) {
-            PlayLayer::destroyPlayer(player, object);
-        }
-    }
-};
-
-class $modify(Speedhack, GJBaseGameLayer) {
-public:
-    void update(float dt) {
-        GJBaseGameLayer::update(dt * speedhackValue);
-    }
-};
 $on_mod(Loaded) {
 	ImGuiCocos::get().setup([] {
 		ImGui::StyleColorsLight();
 	}).draw([] {
-		ImGui::Begin("Polo", nullptr, ImGuiWindowFlags_NoCollapse);
-		if (ImGui::BeginTabBar("Polo")) {
+		ImGui::Begin("Mod Menu", nullptr, ImGuiWindowFlags_NoCollapse);
+		if (ImGui::BeginTabBar("ModTabs")) {
 			if (ImGui::BeginTabItem("Global")) {
 				ImGui::Text("speedhack");
-				ImGui::SliderFloat("##speedhack", &speedhackValue, 0.0001f, 10000.0f, "%.5f");
+				ImGui::SameLine();
+				ImGui::InputFloat("##speedhack", &speedhackValue, 0.0f, 0.0f, "%.5f");
 				ImGui::EndTabItem();
 			}
 			if (ImGui::BeginTabItem("Creator")) {
